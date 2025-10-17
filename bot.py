c2711dc6cb03e72919e8b467a689deb8cfd8e4bf
badalyanyervand <badalyanyervand@gmail.com>
2025-10-17 23:27:20 +0400

    bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ logging.basicConfig(
 log = logging.getLogger("Y-TAXI")
 
 # ====== CONFIG ======
-BOT_TOKEN = os.getenv("BOT_TOKEN", "ԱՅՍՏԵՂ_ԴԻՐ_ՔՈ_TOKENԸ")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8473629116:AAHmkdxdxnAmW58KQaZdE2eC05rwsmUI4wE")
 TELEGRAM_USERNAME = "yandexgopartner"
 PHONE = "+37477554677"
 FORM_URL = "https://forms.gle/tKVJgHu1KCNZhCvRA"
